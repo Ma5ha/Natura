3daa99e2267446c30ed923a13fba4b5a4639c9dd
Ma5ha <blagmash@gmail.com>
2024-12-16 17:49:16 +0100

    add: build ready

--- src/app/[locale]/application/page.tsx
+++ src/app/[locale]/application/page.tsx
@@ -1,12 +1,9 @@
-import { Data } from "@react-google-maps/api";
 import FamilyState from "./(sections)/family-state";
 import HealthAndFunctionality from "./(sections)/health-and-functionality";
 import PersonalData from "./(sections)/personal-data";
 import DataAboutApplication from "./(sections)/data-about-application";
 
 const Application: React.FC = () => {
-  const formatMessage = (id: string) => id;
-
   return (
     <section className="page snap-center flex gap-4">
       <PersonalData />
