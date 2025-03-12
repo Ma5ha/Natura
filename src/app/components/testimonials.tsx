56bc595575b1f051748fde68773439b080b72650
Ma5ha <blagmash@gmail.com>
2025-03-12 12:35:14 +0100

    add: testimonials

--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -1,75 +1,64 @@
 import { typography } from "@/ui/variants";
+import { getTranslations } from "next-intl/server";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-const additionalTestimonials = [
-  {
-    content: [
-      "Ja sam u Domu za starija lica “Natura” bio 6 mjeseci. Tijekom svog boravka, imao sam svu potrebnu pomoć i skrb ustanove. Osoblje je vrlo ljubazno i stručno, a uprava je uvijek spremna da pomogne u svakoj situaciji.",
-    ],
-    author: "Ivan Milić",
-    title: 'Hvala "Natura"!',
-    job: "Bivši klijent",
-  },
-];
+const generateRandom = () => {
+  const id: number[] = [];
+  while (true) {
+    const number = Math.floor(Math.random() * 6);
+    if (!id.includes(number)) id.push(number);
+    if (id.length === 3) break;
+  }
 
-const testimonials = [
-  {
-    content: [
-      "O Domu za starija lica “Natura” mogu reći sve najbolje. Uprava profesionalno rukovodi radom doma uz, isto tako, stručno i ljubazno osoblje koje pruža vrhunsku njegu, što se jednako odnosi na ishranu i higijenu.",
-    ],
-    author: "Neven Pecelj",
-    job: "Posjetilac",
-    title: "Profesionalna i stručna uprava",
-  },
-  {
-    content: [
-      "Već devet mjeseci je ovaj dom razlog zbog kojeg moj deda još uvijek ima snage da se bori i živi za svaki dan. Prije svega, Daliborka, ali i ostatak ekipe imaju savršen, kako profesionalni tako i ljudski pristup, što je meni najvažnije. Hvala vam od srca na tome.",
-    ],
-    author: "Maja Č.",
-    title: "Ljubazan tim",
-    job: "Posjetilac",
-  },
-];
-const Testimonial = () => {
+  return id;
+};
+const Testimonial = async () => {
+  const id = generateRandom();
+  const t = await getTranslations();
+  const testimonials: Array<{
+    title: string;
+    body: string[];
+    signature: string;
+  }> = t.raw("testimonials");
+
+  const selectedTestimonials = id.map((id) => testimonials[id]);
   return (
-    <div className="snap-start py-10 w-full flex justify-center items-center flex-wrap  bg-primary-900 px-5">
+    <div className="snap-start py-10 w-full flex justify-center items-center flex-wrap  bg-gray-600 px-5">
       <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center">
-        {testimonials
-          .concat(additionalTestimonials)
-          .map(({ content, author, job, title }) => (
-            <div
-              key={author}
-              className={"card bg-white max-w-[40ch] flex flex-col  gap-2"}
+        {selectedTestimonials.map(({ body, title, signature }) => (
+          <div
+            key={title}
+            className={"card bg-white max-w-[40ch] flex flex-col  gap-2"}
+          >
+            <h4
+              className={typography({
+                color: "black",
+                size: "large",
+                weight: "bold",
+              })}
             >
-              <h4
+              {title}
+            </h4>
+            {body.map((p) => (
+              <p key={p} className={twMerge(typography(), "indent-4 ")}>
+                {p}
+              </p>
+            ))}
+
+            <div className=" bg-gray-100 rounded-lg px-2 py-1 w-fit mt-auto">
+              <span
                 className={typography({
                   color: "black",
-                  size: "large",
                   weight: "bold",
+                  size: "medium",
                 })}
               >
-                {title}
-              </h4>
-              {content.map((p, i) => (
-                <p key={i} className={twMerge(typography(), "indent-4 ")}>
-                  {p}
-                </p>
-              ))}
-              <div className=" bg-gray-100 rounded-lg px-2 py-1 w-fit mt-auto">
-                <span
-                  className={typography({
-                    color: "black",
-                    weight: "bold",
-                    size: "medium",
-                  })}
-                >
-                  {author}
-                </span>
-                <div className={twMerge(typography(), "text-xs ")}>{job}</div>
-              </div>
+                {signature}
+              </span>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   );
